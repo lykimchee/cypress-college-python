dbc9c3db02fc703d938479ba8db64a1703cb395c
Lynn Kim <lchaeryn5863@gmail.com>
2020-10-23 22:20:54 -0700

    Add files via upload

---
+++ labs/lab1.py
@@ -0,0 +1,18 @@
+def print_first_name():
+    print("Lynn")
+
+
+def print_last_name():
+    print("Kim")
+
+
+def print_name():
+    print_first_name()
+    print_last_name()
+
+
+def main():
+    print_name()
+
+
+main()
---
+++ labs/lab2.py
@@ -0,0 +1,10 @@
+def main():
+    full_name = "Lynn Kim"
+    first_name = full_name[:4]
+    last_name = full_name.replace("Lynn ", "")
+    print(full_name)
+    print(first_name)
+    print(last_name)
+
+
+main()
